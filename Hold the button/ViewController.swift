fba205dd32ffc0eaaa9bf7c506db04fd33b36ebc
Igor Palyvoda <tib.igor.mk@gmail.com>
2019-05-14 09:53:47 +0300

    Initial Commit

---
+++ Hold the button/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hold the button
+//
+//  Created by Ingvar on 5/14/19.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
